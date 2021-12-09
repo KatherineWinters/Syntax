494707227311bde1cc5f95427037f59f0c0cb099
Katherine Winters <goble.katherine@gmail.com>
2021-12-08 19:07:53 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SyntaxSugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int answer = 4;
+            string response;
+
+            if (answer < 9)
+            {
+                response = $"{answer} is less than nine";
+            }
+            else
+            {
+                response = $"{answer} is greater than nine";
+            }
+            Console.WriteLine(response);
+        }
+    }
+}
